c7093e2e6918ddf63000a967a804be40a7d67284
Dari Alves <darieltondf@gmail.com>
2021-03-09 21:21:39 -0300

    cadastro de conta

--- chapter-I/primeiro-projeto-nodejs/src/index.js
+++ chapter-I/primeiro-projeto-nodejs/src/index.js
@@ -1,5 +1,26 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
 
-app.listen(3333);
+const customers = [];
+
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body;
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+
+    return response.status(201).send();
+
+});
+
+app.listen(3333, () => {
+    console.log('Server listening on port 3333');
+});
