544614e381c5b45084c4e153e1102d2bd6fe6274
Dari Alves <darieltondf@gmail.com>
2021-03-09 21:36:19 -0300

    validando cpf existente

--- chapter-I/primeiro-projeto-nodejs/src/index.js
+++ chapter-I/primeiro-projeto-nodejs/src/index.js
@@ -8,17 +8,21 @@ const customers = [];
 
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
-    const id = uuidv4();
+
+    const customerAlreadyExists = customers.some(customer => customer.cpf === cpf);
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: 'Customer already exists!' });
+    }
 
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     });
 
     return response.status(201).send();
-
 });
 
 app.listen(3333, () => {
