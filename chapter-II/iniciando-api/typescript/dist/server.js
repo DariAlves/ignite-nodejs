1ae42cc284faddc24fbdf2a6935b62669d32d43b
Dari Alves <darieltondf@gmail.com>
2021-03-15 21:13:37 -0300

    add types

--- chapter-II/iniciando-api/typescript/dist/server.js
+++ chapter-II/iniciando-api/typescript/dist/server.js
@@ -4,10 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var routes_1 = require("./routes");
 var app = express_1.default();
-app.get('/', function (request, response) {
-    return response.json({ message: 'hello world' });
-});
+app.get('/', routes_1.createCourse);
 app.listen(3333, function () {
     console.log('Server listening on port 3333');
 });
--- chapter-II/iniciando-api/typescript/src/server.ts
+++ chapter-II/iniciando-api/typescript/src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express';
+import { createCourse } from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'hello world' });
-});
+app.get('/', createCourse);
 
 app.listen(3333, () => {
     console.log('Server listening on port 3333');
